0023417272ffcf22482e0e77a48e490117d028eb
novoneel-iudx <novoneel.chakraborty@datakaveri.org>
2024-11-19 15:23:48 +0530

    [REFACTOR]: Version number - minor uptick

--- src/cdpg_anonkit/__init__.py
+++ src/cdpg_anonkit/__init__.py
@@ -2,7 +2,7 @@
 A toolkit for data anonymisation.
 View the documentation for this project [here](https://novoneel-iudx.github.io/differential-privacy-toolkit/).
 """
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 
 '''Functions to be exposed to user'''
